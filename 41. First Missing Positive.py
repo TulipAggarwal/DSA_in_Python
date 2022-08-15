06cfdcfee4129a144e07c0d08b05313acda13c04
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-15 22:53:31 +0530

    Delete 41. First Missing Positive.py

--- 41. First Missing Positive.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        length = len(nums)
-        var = nums
-        hash = set(var)
-        for i in range(1,length+2):
-            if i not in hash:
-                return i
-            
